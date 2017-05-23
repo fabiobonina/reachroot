73fea2cdda162fff33d884bc7ee9f72ea4a1309d
Fabio Bonina <fabiobonina@gmail.com>
2017-05-23 07:15:13 -0300

    teste

--- src/pages/Tchat.vue
+++ src/pages/Tchat.vue
@@ -4,7 +4,20 @@
 
 <script>
 
+    import Sidebar from '../components/sidebar/Sidebar'
+    import Messages from '../components/messages/Messages'
+    
+    export default {
+        name: 'tchat',
+        components: { Sidebar, Messages }
+    }
+
 </script>
 
-<style>
+<style scoped>
+
+.tchat__container{
+    height: 100%;
+}
+
 </style>
